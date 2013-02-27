e15769e9470cd2f653cf5821bb363f75f7126a69
Alan deLevie <adelevie@gmail.com>
2013-02-27 11:14:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloud66App::Application.config.secret_token = '27e169d6d6cd90e52d3bd994e1aacf83784924ad7e5186454725d4180725e901daa37e2b7932307456643bc16b41476257d32e70f6e035d14316ff92eb31a56f'
